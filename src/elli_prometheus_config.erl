1a0147f2fcdbc9589ae9e72f75c3742fac4b81e5
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-14 01:38:07 +0300

    introduce configurable labels, make status_class default and status_code optional. close #4

--- src/elli_prometheus_config.erl
+++ src/elli_prometheus_config.erl
@@ -2,16 +2,19 @@
 
 -export([path/0,
          format/0,
-         duration_buckets/0]).
+         duration_buckets/0,
+         labels/0]).
 
 -define(DEFAULT_PATH, <<"/metrics">>).
 -define(DEFAULT_FORMAT, prometheus_text_format).
 -define(DEFAULT_DURATION_BUCKETS, [10,100,1000,10000,100000,300000,500000,
                                    750000,1000000,1500000,2000000,3000000]).
+-define(DEFAULT_LABELS, [method,handler,status_class]).
 
 -define(DEFAULT_CONFIG, [{path,?DEFAULT_PATH},
                          {format,?DEFAULT_FORMAT},
-                         {duration_buckets,?DEFAULT_DURATION_BUCKETS}]).
+                         {duration_buckets,?DEFAULT_DURATION_BUCKETS},
+                         {labels,?DEFAULT_LABELS}]).
 
 config() ->
   application:get_env(prometheus, elli_exporter, ?DEFAULT_CONFIG).
@@ -27,3 +30,7 @@ format() ->
 duration_buckets() ->
   Config = config(),
   proplists:get_value(duration_buckets, Config, ?DEFAULT_DURATION_BUCKETS).
+
+labels() ->
+  Config = config(),
+  proplists:get_value(labels, Config, ?DEFAULT_LABELS).
