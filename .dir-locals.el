15e65fea4a218943f293c4a3e676aa50c98fa1d8
Eric Bailey <eric@ericb.me>
2016-08-13 19:29:20 -0500

    Turn on fci-mode by default

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ((nil         . ((indent-tabs-mode             . nil)
-                 (fill-column                  . 80)))
+                 (fill-column                  . 80)
+                 (eval                         . (turn-on-fci-mode))))
  (erlang-mode . ((flycheck-erlang-library-path . ("../_build/test/lib/elli"
                                                   "../_build/test/lib/elli/ebin"))
                  (erlang-indent-level          . 2))))
