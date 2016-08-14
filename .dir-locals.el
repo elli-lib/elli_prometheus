ab48b74682e1b89a787cfa5e77c4fa168464992e
Eric Bailey <eric@ericb.me>
2016-08-13 19:26:23 -0500

    Ensure Flycheck can find Elli

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil         . ((indent-tabs-mode             . nil)
                  (fill-column                  . 80)))
- (erlang-mode . ((flycheck-erlang-include-path . ("../_build/test/lib"))
-                 (flycheck-erlang-library-path . ("../_build/test/lib/*/ebin"))
+ (erlang-mode . ((flycheck-erlang-library-path . ("../_build/test/lib/elli"
+                                                  "../_build/test/lib/elli/ebin"))
                  (erlang-indent-level          . 2))))
