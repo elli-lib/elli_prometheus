61058e4f32c122f4f73dc487192219fb3631e046
Ilya Khaprov <dead.trickster@gmail.com>
2016-09-22 02:56:27 +0300

    all metrics work for basic test

--- src/elli_prometheus.erl
+++ src/elli_prometheus.erl
@@ -17,7 +17,7 @@
 -define(REQUEST_HEADERS_DURATION, http_request_headers_microseconds).
 -define(REQUEST_BODY_DURATION, http_request_body_microseconds).
 -define(REQUEST_USER_DURATION, http_request_user_microseconds).
--define(REQUEST_SEND_DURATION, http_response_send_microseconds).
+-define(RESPONSE_SEND_DURATION, http_response_send_microseconds).
 
 -define(RESPONSE_SIZE, http_response_size_bytes).
 -define(RESPONSE_HEADERS_SIZE, http_response_headers_size_bytes).
@@ -50,10 +50,39 @@ handle_event(elli_startup, _Args, _Config) ->
   Labels        = elli_prometheus_config:labels(),
   Buckets       = elli_prometheus_config:duration_buckets(),
   RequestCount  = metric(?TOTAL, Labels, "request count"),
-  ExecutionTime = metric(?REQUEST_DURATION, [response_type | Labels], Buckets,
-                         "execution time"),
+  RequestDuration = metric(?REQUEST_DURATION, [response_type | Labels], Buckets,
+                           " latencies in microseconds"),
+  RequestHeadersDuration = metric(?REQUEST_HEADERS_DURATION,
+                                  Labels, Buckets,
+                                  "time spent receiving and parsing headers"),
+  RequestBodyDuration = metric(?REQUEST_BODY_DURATION,
+                               Labels, Buckets,
+                               "time spent receiving and parsing body"),
+  RequestUserDuration = metric(?REQUEST_USER_DURATION,
+                               Labels, Buckets,
+                               "time spent in user callback"),
+  ResponseSendDuration = metric(?RESPONSE_SEND_DURATION,
+                                [response_type | Labels], Buckets,
+                                "time spent sending reply"),
+  ResponseSize = metric(?RESPONSE_SIZE,
+                        [response_type | Labels],
+                        "total response size"),
+  ResponseHeaders = metric(?RESPONSE_HEADERS_SIZE,
+                           [response_type | Labels],
+                           "response headers size"),
+  ResponseBody = metric(?RESPONSE_BODY_SIZE,
+                        [response_type | Labels],
+                        "response body size"),
   prometheus_counter:declare(RequestCount),
-  prometheus_histogram:declare(ExecutionTime),
+  prometheus_histogram:declare(RequestDuration),
+  prometheus_histogram:declare(RequestHeadersDuration),
+  prometheus_histogram:declare(RequestBodyDuration),
+  prometheus_histogram:declare(RequestUserDuration),
+
+  prometheus_histogram:declare(ResponseSendDuration),
+  prometheus_summary:declare(ResponseSize),
+  prometheus_summary:declare(ResponseHeaders),
+  prometheus_summary:declare(ResponseBody),
   ok;
 handle_event(_Event, _Args, _Config) -> ok.
 
@@ -72,21 +101,21 @@ handle_full_response(Type, [Req,Code,_Hs,_B,{Timings, Sizes}], _Config) ->
 
   prometheus_histogram:observe(?REQUEST_DURATION, TypedLabels,
                                duration(Timings, request)),
-  %% prometheus_histogram:observe(?REQUEST_HEADERS_DURATION, Labels,
-  %%                              duration(Timings, headers)),
-  %% prometheus_histogram:observe(?REQUEST_BODY_DURATION, Labels,
-  %%                              duration(Timings, body)),
-  %% prometheus_histogram:observe(?REQUEST_USER_DURATION, Labels,
-  %%                              duration(Timings, user)),
-  %% prometheus_histogram:observe(?REQUEST_SEND_DURATION, TypedLabels,
-  %%                              duration(Timings, send)),
+  prometheus_histogram:observe(?REQUEST_HEADERS_DURATION, Labels,
+                               duration(Timings, headers)),
+  prometheus_histogram:observe(?REQUEST_BODY_DURATION, Labels,
+                               duration(Timings, body)),
+  prometheus_histogram:observe(?REQUEST_USER_DURATION, Labels,
+                               duration(Timings, user)),
+  prometheus_histogram:observe(?RESPONSE_SEND_DURATION, TypedLabels,
+                               duration(Timings, send)),
 
-  %% prometheus_summary:observe(?RESPONSE_SIZE, Labels,
-  %%                            size(Sizes, response)),
-  %% prometheus_summary:observe(?RESPONSE_HEADERS_SIZE, Labels,
-  %%                            size(Sizes, response_headers)),
-  %% prometheus_summary:observe(?RESPONSE_BODY_SIZE, Labels,
-  %%                            size(Sizes, response_body)),
+  prometheus_summary:observe(?RESPONSE_SIZE, TypedLabels,
+                             size(Sizes, response)),
+  prometheus_summary:observe(?RESPONSE_HEADERS_SIZE, TypedLabels,
+                             size(Sizes, response_headers)),
+  prometheus_summary:observe(?RESPONSE_BODY_SIZE, TypedLabels,
+                             size(Sizes, response_body)),
   ok.
 
 format_metrics() ->
@@ -100,9 +129,9 @@ duration(Timings, headers) ->
 duration(Timings, body) ->
   duration(body_start, body_end, Timings);
 duration(Timings, user) ->
-  duration(user_end, user_start, Timings);
+  duration(user_start, user_end, Timings);
 duration(Timings, send) ->
-  duration(send_end, send_start, Timings).
+  duration(send_start, send_end, Timings).
 
 duration(StartKey, EndKey, Timings) ->
   Start = proplists:get_value(StartKey, Timings),
--- test/elli_prometheus_tests.erl
+++ test/elli_prometheus_tests.erl
@@ -15,11 +15,11 @@ setup() ->
   application:start(ssl),
   inets:start(),
   prometheus:start(),
-  
+
   %% force elli_middleware module loading
   %% otherwise elli won't recognize it as a callback
   %% when erlang runs in interactive mode.
-  elli_middleware:module_info(), 
+  elli_middleware:module_info(),
   Config = [
             {mods, [
                     {elli_prometheus, []},
@@ -42,12 +42,47 @@ init_stats() ->
 clear_stats(_) ->
   ets:delete(elli_stat_table).
 
+histogram_count_sum(Name, Labels) ->
+  {Buckets, Sum} = prometheus_histogram:value(Name, Labels),
+  {lists:sum(Buckets), Sum}.
+
+counter_value(Name, Labels) ->
+  prometheus_counter:value(Name, Labels).
+
+summary_value(Name, Labels) ->
+  prometheus_summary:value(Name, Labels).
+
 hello_world() ->
   {ok, Response} = httpc:request("http://localhost:3001/hello/world"),
   ?assertMatch(200, status(Response)),
   ?assertMatch([{"connection", "Keep-Alive"},
                 {"content-length", "12"}], headers(Response)),
-  ?assertMatch("Hello World!", body(Response)).
+  ?assertMatch("Hello World!", body(Response)),
+  %%?debugVal(ets:tab2list(prometheus_histogram_table)),
+  Labels = ['GET', <<"hello">>, "success"],
+  ?assertMatch(1, counter_value(http_requests_total, Labels)),
+  ?assertMatch({1, S} when S > 0,
+                           histogram_count_sum(http_request_duration_microseconds,
+                                               ["full" | Labels])),
+  ?assertMatch({1, S} when S > 0,
+                           histogram_count_sum(http_request_headers_microseconds,
+                                               Labels)),
+  ?assertMatch({1, S} when S > 0,
+                           histogram_count_sum(http_request_body_microseconds,
+                                               Labels)),
+  ?assertMatch({1, S} when S > 0,
+                           histogram_count_sum(http_request_user_microseconds,
+                                               Labels)),
+
+  ?assertMatch({1, S} when S > 0,
+                           histogram_count_sum(http_response_send_microseconds,
+                                               ["full" | Labels])),
+  ?assertMatch({1, 75}, summary_value(http_response_size_bytes,
+                                      ["full" | Labels])),
+  ?assertMatch({1, 63}, summary_value(http_response_headers_size_bytes,
+                                      ["full" | Labels])),
+  ?assertMatch({1, 12}, summary_value(http_response_body_size_bytes,
+                                      ["full" | Labels])).
 
 %%% Helpers
 
